e1cda03951d7b7ac165588be05c813baa84770a3
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-07-05 05:30:31 -0600

    Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1>Welcome to the React App</h1>
+      <h1 className="text-3xl">Welcome to React!</h1>
       <p>This is a simple React application.</p>
       <p>Feel free to modify it as you like!</p>
     </>
