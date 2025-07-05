cb27ee8e49617bf65b485c9c293829d2706b7add
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-07-05 05:16:22 -0600

    Initialize React application with Vite and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+      <p>Feel free to modify it as you like!</p>
+    </>
+  )
+}
+
+export default App
