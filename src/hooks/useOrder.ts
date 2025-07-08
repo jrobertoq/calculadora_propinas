e1f56d092ac7e69baee2ddd4ee0edbd4d911da14
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-07-08 17:08:49 -0600

    Adds item to order

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 
-import type { OrderItem } from '../types'
+import type { MenuItem, OrderItem } from '../types'
 
 export default function useOrder() {
     const [order , setOrder] = useState<OrderItem[]>([])
 
-    const addItem = () => {
-        console.log('Adding item to order:')
+    const addItem = (item: MenuItem) => {
+        const newItem = {...item, quantity: 1}
+        setOrder([...order, newItem])
     }
-
+    
     return {
         addItem  
     }
