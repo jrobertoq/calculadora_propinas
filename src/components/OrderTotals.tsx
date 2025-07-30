8638d0c6f001177f83eb1dcaa58ba8054a47f866
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-07-30 09:43:02 -0600

    Calculates and displays the total order amount

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -11,6 +11,7 @@ export default function OrderTotals({ order, tip }: OrderTotalsProps) {
 
     const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.price * item.quantity), 0), [order])
     const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
+    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
 
     return (
     <>
@@ -28,7 +29,7 @@ export default function OrderTotals({ order, tip }: OrderTotalsProps) {
             </p>
             <p>Total a pagar: {''}
                 <span className="font-bold">
-                    {formatCurrency(subtotalAmount * 1.15)}
+                    {formatCurrency(totalAmount)}
                 </span>
             </p>  
         </div>
